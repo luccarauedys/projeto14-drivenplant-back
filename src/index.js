90235b67ccccb6205e389114e55cdadb8b2fe559
carolxavier <xavier.biotec@gmail.com>
2022-05-12 17:46:09 -0300

    Starting code for POST/cart and GET/cart

--- src/index.js
+++ src/index.js
@@ -29,12 +29,43 @@ app.get('/product', async (req,res) => {
       res.sendStatus(404)
       return;
     }
-    console.log(product, "CADE");
     res.status(200).send(product);
   } catch (error){
     res.sendStatus(500);
   }
-})
+});
+
+//Quando clicar em "adicionar ao carrinho"
+app.post('/cart', async (req,res) => {
+  const {id} = req.query;
+  const { user } = req.headers;
+  //verificar se o usuário está logado
+  // try{
+  //   const products = db.collection("products");
+  //   const product = await products.findOne({id: id});
+
+  //   const users = db.collection("users");
+  //   const userCart = users.updateOne(
+  //     {id:id}, {$set: {cart: [...cart, product]}}
+  //   );
+  // }catch (error){
+  //   res.sendStatus(500);
+  // }
+});
+
+app.get('/cart', (req,res) => {
+  const { user } = req.headers;
+  //verificar se o usuário está logado
+  // try{
+  //     const users = db.collection("users");
+  //     const client = await users.findOne({name: user});
+  //     const {cart} = client;
+  //     console.log("VER O CARRINHO", cart)
+  //     res.status(200).send(cart);
+  // }catch (error){
+  //   res.sendStatus(500);
+  // }
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor em pé na porta", process.env.PORT);

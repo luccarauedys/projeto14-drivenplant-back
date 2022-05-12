d199c117420beacc8a001dff2116e89b0f6219d8
carolxavier <xavier.biotec@gmail.com>
2022-05-12 12:44:00 -0300

    DB "drivenPlant" created and connected to MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { MongoClient } from 'mongodb';
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express().use(cors()).use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db = null;
+const promise = mongoClient.connect();
+promise.then(() => {
+    db = mongoClient.db("drivenPlant");
+    console.log("Seu DB está funcionando! YAY");
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor em pé na porta", process.env.PORT);

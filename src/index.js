1e646abfc12d87f24662e8533498ed5dcc817a22
carolxavier <xavier.biotec@gmail.com>
2022-05-12 15:03:13 -0300

    GET /products

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ try {
   res.status(500).send("Não foi possível conectar ao DB");
 }    
 
-const products = await db.collection("products").insertMany([
+await db.collection("products").insertMany([
   {name:"Columéia Peixinho", image:"https://http2.mlstatic.com/D_NQ_NP_986427-MLB27617620809_062018-O.webp", price:21.90, category:"suculenta", info:"Facilmente cultivada em ambientes internos pois prefere locais de meia-sombra ou luz difusa"},
   {name:"Jade Chinês", image:"https://i.pinimg.com/originals/b6/4c/69/b64c69c15e01681f056b3969cc306c71.jpg", price:25.00, category:"suculenta", info:"Prefere locais com bastante luminosidade, podendo deixa-la exposta ao sol por algumas horas do dia"},
   {name:"Pata de urso", image:"https://www.orquidario4e.com.br/Content/images/product/00018256_1_m_d_00.jpg", price:14.90, category:"suculenta", info:"Gosta de clima subtropical ou tropical e prefere ser cultivada em locais de meia sombra com bastante luminosidade"},
@@ -24,7 +24,10 @@ const products = await db.collection("products").insertMany([
   {name:"Muda de Ipê Rosa", image:"https://http2.mlstatic.com/D_NQ_NP_778128-MLB46016634797_052021-O.jpg", price:48.00, category:"paisagismo", info:"Floresce abundantemente de junho a agosto, e prefere climas mais quentes"},
   {name:"Muda de Eucalipto Arco-Íris", image:"https://static3.tcdn.com.br/img/img_prod/350075/muda_de_eucalipto_arco_iris_feita_de_semente_fc_15033_1_d0fc086e243950a207df24c4d4a3c083_20220412114305.jpg", price:26.00, category:"paisagismo", info:"No seu habitat pode atingir 75 metros de altura, mas em cultivo geralmente permanece entre 20 a 30 metros de altura"}
 ]);
-console.log(products);
+
+app.get('/products', (req, res) => {
+  db.collection("products").find({}).toArray().then(products => res.send(products));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor em pé na porta", process.env.PORT);

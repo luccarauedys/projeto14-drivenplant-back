adcb8c7e5807958198bedc8e501c9c9e184d36ec
Lucca Rauédys <luccarauedys@outlook.com>
2022-05-15 10:11:41 -0300

    alterando link do db

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 let db = null;
 try {
-  // const mongoClient = new MongoClient(process.env.MONGO_URI);
-  const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+  const mongoClient = new MongoClient(process.env.MONGO_URI);
   await mongoClient.connect();
   db = mongoClient.db("drivenplant");
 } catch (error) {
